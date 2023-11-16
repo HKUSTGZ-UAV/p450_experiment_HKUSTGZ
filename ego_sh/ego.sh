947745c8a634d74e3cf80e89b2a61998d3011c88
Eason Yi <eason473867143@gmail.com>
2023-11-16 18:57:02 +0800

    remove all code

--- ego_sh/ego.sh
+++
@@ -1,6 +0,0 @@
-## ego自主飞行脚本
-gnome-terminal --window -e 'bash -c "roslaunch p450_experiment p450_mavros.launch; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch realsense2_camera rs_d400_and_t265.launch; exec bash"' \
---tab -e 'bash -c "sleep 15; roslaunch prometheus_gazebo sitl_ego_planner.launch; exec bash"' \
---tab -e 'bash -c "sleep 20; roslaunch prometheus_gazebo sitl_ego_station.launch; exec bash"' \
-
--- launch_detection/siamrpn_with_rtsp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gnome-terminal --window -e 'bash -c "roslaunch p450_experiment mipi_veye327.launch; exec bash"' \
---tab -e 'bash -c "sleep 5; source /home/amov/catkin_workspace/install/setup.bash --extend; roslaunch p450_experiment siamRPN.launch; exec bash"' \
---tab -e 'bash -c "sleep 15; roslaunch p450_experiment rtsp.launch; exec bash"' \
