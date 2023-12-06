52c3a21c162d941cae473b188723d600442919cb
Eason Yi <eason473867143@gmail.com>
2023-12-06 11:48:56 +0800

    add lidar_ego_indoor

--- src/p450_experiment/sh_for_p450/lidar_ego_indoor.sh
+++ src/p450_experiment/sh_for_p450/lidar_ego_indoor.sh
@@ -4,5 +4,5 @@ gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch p450_experiment LDS-50C-3.launch; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch p450_experiment filter_lidar.launch; exec bash"' \
 --tab -e 'bash -c "sleep 8; roslaunch p450_experiment scan_to_octomap.launch; exec bash"' \
---tab -e 'bash -c "sleep 10; roslaunch p450_experiment ego_planner_depth.launch; exec bash"' \
+--tab -e 'bash -c "sleep 10; roslaunch p450_experiment ego_planner_octomap.launch; exec bash"' \
 --tab -e 'bash -c "sleep 12; source ~/.bashrc; roslaunch p450_experiment rviz_2dlidar.launch; exec bash"' \
