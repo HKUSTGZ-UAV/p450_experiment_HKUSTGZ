5bd8ea746007b2d62ef68865bbdc29863c307bf4
Eason Yi <eason473867143@gmail.com>
2024-03-11 17:00:19 +0800

    add t265-usb3.0 autoload

--- src/p450_experiment/sh_for_p450/p450_autoload.sh
+++ src/p450_experiment/sh_for_p450/p450_autoload.sh
@@ -1,1 +1,6 @@
+# 用于驱动T265-USB3.0接口，相当于插拔一次
+gnome-terminal --window -e 'bash -c "EnableGpio_for_USB.sh"' \
+--tab -e 'bash -c "sleep 3;EnableGpio_for_USB.sh"' \
+
+# 启动通信节点
 gnome-terminal --window -e 'bash -c "source /opt/ros/noetic/setup.bash && source ~/p450_experiment/devel/setup.bash && roslaunch p450_experiment P450_indoor_communication.launch; exec bash"' \
