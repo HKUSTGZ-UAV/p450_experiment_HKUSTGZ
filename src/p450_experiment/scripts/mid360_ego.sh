14da23ee49d85c2c48463c23293f49b6fcb82de1
Zhang__Ling <a1544839693@163.com>
2025-02-10 16:55:27 +0800

    update Prometheus and add mid360_to_octomap.launch

--- src/p450_experiment/scripts/mid360_ego.sh
+++ src/p450_experiment/scripts/mid360_ego.sh
@@ -1,2 +1,3 @@
-gnome-terminal --window -e 'bash -c "sleep 1; roslaunch p450_experiment ego_planner_basic_mid360.launch; exec bash"' \
+gnome-terminal --window -e 'bash -c "sleep 1; roslaunch p450_experiment mid360_to_octomap.launch; exec bash"' \
+--tab -e 'bash -c "sleep 2; roslaunch p450_experiment ego_planner_basic_mid360.launch; exec bash"' \
 --tab -e 'bash -c "sleep 3; roslaunch p450_experiment rviz_mid360.launch; exec bash"' \
